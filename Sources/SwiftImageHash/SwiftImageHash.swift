6668945bfe68a3a8836bcc5b40be04d19ff26313
shigashi <shigashi@R-MBP-14JBL-0824.local>
2024-05-22 22:02:58 +0900

    add comments

--- Sources/SwiftImageHash/SwiftImageHash.swift
+++ Sources/SwiftImageHash/SwiftImageHash.swift
@@ -2,12 +2,19 @@
 import Foundation
 import UIKit
 
+/// `SwiftImageHash` provides functionality to compute perceptual hashes of images and to compare these hashes.
+///
+/// Perceptual hashing allows you to generate a fingerprint of an image based on its visual content.
 public struct SwiftImageHash {
+    /// Initializes a new instance of `SwiftImageHash`.
     public init() {}
 
-    /// Compute Perceptual Hashing
-    /// - Parameter image: UIImage
-    /// - Returns: phash in UInt64
+    /// Computes the perceptual hash (pHash) of an image.
+    ///
+    /// This method first converts the image to grayscale and resizes it. It then applies a 2D Discrete Cosine Transform (DCT) to the resulting image buffer. The pHash is generated based on the DCT coefficients.
+    ///
+    /// - Parameter image: The `UIImage` instance for which to compute the pHash.
+    /// - Returns: A `String` representing the pHash of the image, or `nil` if the image could not be processed.
     public static func phash(image: UIImage) -> String? {
         guard let imageBuf = grayscaleAndResize(image: image) else {
             return nil
@@ -20,6 +27,14 @@ public struct SwiftImageHash {
         return generatePHash(from: dctCoefficients)
     }
     
+    /// Calculates the Hamming distance between two perceptual hashes.
+    ///
+    /// Hamming distance is the number of positions at which the corresponding symbols are different. In the context of pHashes, it measures how many bits are different between two hash values.
+    ///
+    /// - Parameters:
+    ///   - phash1: The first pHash as a hexadecimal `String`.
+    ///   - phash2: The second pHash as a hexadecimal `String`.
+    /// - Returns: An `Int` representing the Hamming distance between the two hashes, or `nil` if either hash string could not be converted to a UInt64.
     public static func distanceBetween(_ phash1: String, _ phash2: String) -> Int? {
         guard let hexValue1 = hexToUInt64(phash1) else {
             return nil
@@ -31,22 +46,25 @@ public struct SwiftImageHash {
         
         return hammingDistanceBetween(hexValue1, hexValue2)
     }
-}
-
-func generatePHash(from dctCoefficients: [Float]) -> String {
-    let significantCoefficients = Array(dctCoefficients.dropFirst()) // Drop DC component if present
-    let mean = significantCoefficients.reduce(0, +) / Float(significantCoefficients.count)
     
-    let hashBits = dctCoefficients.map { $0 > mean ? 1 : 0 }
-    
-    var result: UInt64 = 0
-    for (index, bit) in hashBits.enumerated() {
-        if bit == 1 {
-            result |= (1 << (63 - index))
+    /// Generates a perceptual hash from DCT coefficients.
+    /// - Parameter coefficients: The DCT coefficients.
+    /// - Returns: A hexadecimal string representing the pHash.
+    private static func generatePHash(from dctCoefficients: [Float]) -> String {
+        let significantCoefficients = Array(dctCoefficients.dropFirst()) // Drop DC component if present
+        let mean = significantCoefficients.reduce(0, +) / Float(significantCoefficients.count)
+        
+        let hashBits = dctCoefficients.map { $0 > mean ? 1 : 0 }
+        
+        var result: UInt64 = 0
+        for (index, bit) in hashBits.enumerated() {
+            if bit == 1 {
+                result |= (1 << (63 - index))
+            }
         }
+        return uint64ToHex(result)
     }
-    
-    return uint64ToHex(result)
+
 }
 
 func hammingDistanceBetween(_ num1: UInt64, _ num2: UInt64) -> Int {
